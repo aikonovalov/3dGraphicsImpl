dc5ec8e7cf2ba9834b809b839bd4db80e0fa816a
Artyom Konovalov <aikonovalov@edu.hse.ru>
2025-01-29 21:49:43 +0300

    Parameters manager added

--- Object/Camera.cpp
+++ Object/Camera.cpp
@@ -1,45 +1,108 @@
 #include "Camera.h"
+#include "Holder.h"
 
 namespace Scene {
 
-Camera::Camera()
-    : near_({0, 0, 1}, -1),
-      far_({0, 0, -1}, -1),
-      top_({0, -1, 0}, -1),
-      bottom_({0, 1, 0}, -1),
-      left_({1, 0, 0}, -1),
-      right_({-1, 0, 0}, -1) {}
+Camera::ParametersHandler::ParametersHandler()
+    : fov(90),
+      aspect_ratio(1.7),
+      near_distance(0.1),
+      far_distance(100.0),
+      hfov_sinus(std::sin(fov / 2.0)),
+      hfov_cosinus(std::cos(fov / 2.0)),
+      hfov_tangens(std::tan(fov / 2.0)),
+      focal_length(1 / hfov_tangens) {
+  BuildPlanes();
+}
 
-Camera::Camera(const ElemType x_angle, const ElemType y_angle,
-               const ElemType z_angle)
+Camera::ParametersHandler::ParametersHandler(ElemType fov, ElemType asp_rat,
+                                             ElemType ndist, ElemType fdist,
+                                             ElemType flen)
+    : fov(fov),
+      aspect_ratio(asp_rat),
+      near_distance(ndist),
+      far_distance(fdist),
+      focal_length(flen) {
+  BuildPlanes();
+}
+
+void Camera::ParametersHandler::SetFov(ElemType new_fov) {
+  fov = new_fov;
+  hfov_sinus = std::sin(fov / 2.0);
+  hfov_cosinus = std::cos(fov / 2.0);
+  hfov_tangens = std::tan(fov / 2.0);
+
+  BuildPlanes();
+}
+
+void Camera::ParametersHandler::SetAspect(ElemType new_aspect) {
+  aspect_ratio = new_aspect;
+  BuildPlanes();
+}
+
+void Camera::ParametersHandler::SetNearDistance(ElemType new_nd) {
+  near_distance = new_nd;
+  BuildPlanes();
+}
+
+void Camera::ParametersHandler::SetFarDistance(ElemType new_fd) {
+  far_distance = new_fd;
+  BuildPlanes();
+}
+
+void Camera::ParametersHandler::BuildPlanes() {
+  near = Linear::Plane({0, 0, -1.0}, -near_distance);
+  far = Linear::Plane({0, 0, 1.0}, far_distance);
+  left = Linear::Plane({focal_length, 0, -1.0}, 0);
+  right = Linear::Plane({-focal_length, 0, -1.0}, 0);
+  top = Linear::Plane({0, focal_length, aspect_ratio}, 0);
+  bottom = Linear::Plane({0, -focal_length, aspect_ratio}, 0);
+}
+
+void Camera::ParametersHandler::ConstructTransformMatrix(
+    Linear::Matrix<4, 4>& matrix) {
+  matrix.At(0, 0) = focal_length;
+  matrix.At(1, 1) = near_distance / focal_length;
+  matrix.At(2, 2) =
+      -(far_distance + near_distance) / (far_distance - near_distance);
+  matrix.At(3, 2) = -1.0;
+  matrix.At(2, 3) =
+      -2 * near_distance * far_distance / (far_distance - near_distance);
+}
+
+Camera::Camera() : handler_() {}
+
+Camera::Camera(const ElemType xy_angle, const ElemType yz_angle,
+               const ElemType xz_angle)
     : Camera() {
-  Rotate(x_angle, y_angle, z_angle);
+  Rotate(xy_angle, yz_angle, xz_angle);
 }
 
-void Camera::Rotate(const ElemType x_angle, const ElemType y_angle,
-                    const ElemType z_angle) {
-  Linear::Matrix<4, 4> rotaion =
-      Linear::Matrix<4, 4>::MakeRotationMatrix(x_angle, y_angle, z_angle);
-  top_.Rotate(rotaion);
-  bottom_.Rotate(rotaion);
-  left_.Rotate(rotaion);
-  right_.Rotate(rotaion);
-  near_.Rotate(rotaion);
-  far_.Rotate(rotaion);
+void Camera::Rotate(const ElemType xy_angle, const ElemType yz_angle,
+                    const ElemType xz_angle) {
+  rotation_matrix_ *=
+      Linear::Matrix<4, 4>::MakeRotationMatrix(xy_angle, yz_angle, xz_angle);
 }
 
-const Linear::Matrix<4, 4>& Camera::GetTransformMatrix() {
+void Camera::ResetRotate() {
+  rotation_matrix_ = Linear::Matrix<4, 4>::Eye();
+}
+
+const Linear::Matrix<4, 4>& Camera::GetTransformMatrix() const {
   return transform_matrix_;
 }
 
-void Camera::MakeTransform() {
-  ElemType tangens = std::tan(FOV / 2.0);
-  ElemType dist = farDistance - nearDistance;
-  transform_matrix_.At(0, 0) = 1.0 / (aspectRatio * tangens);
-  transform_matrix_.At(1, 1) = 1.0 / tangens;
-  transform_matrix_.At(2, 2) = -(farDistance + nearDistance) / dist;
-  transform_matrix_.At(2, 3) = -2.0 * farDistance * nearDistance / dist;
-  transform_matrix_.At(3, 2) = -1.0;
+const Linear::Matrix<4, 4>& Camera::GetRotateMatrix() const {
+  return transform_matrix_;
+}
+
+Linear::Triangle Camera::Transform(Linear::Triangle& triangle) {
+  triangle.Transform(transform_matrix_ * rotation_matrix_);
+  return triangle;
+}
+
+void Camera::ConstructTransformMatrix() {
+  handler_.ConstructTransformMatrix(transform_matrix_);
 };
 
 }  // namespace Scene
--- Object/Camera.h
+++ Object/Camera.h
@@ -9,6 +9,38 @@ struct Camera {
   using ElemType = Linear::Plane::ElemType;
 
 public:
+  class ParametersHandler {
+  public:
+    ParametersHandler();
+    ParametersHandler(ElemType, ElemType, ElemType, ElemType, ElemType);
+
+    void SetFov(ElemType);
+    void SetAspect(ElemType);
+    void SetNearDistance(ElemType);
+    void SetFarDistance(ElemType);
+
+    void BuildPlanes();
+
+    void ConstructTransformMatrix(Linear::Matrix<4, 4>&);
+
+  private:
+    ElemType fov;
+    ElemType aspect_ratio;  // TODO: Connect with QT
+    ElemType near_distance;
+    ElemType far_distance;
+    ElemType hfov_sinus;
+    ElemType hfov_cosinus;
+    ElemType hfov_tangens;
+    ElemType focal_length;
+
+    Linear::Plane near;
+    Linear::Plane far;
+    Linear::Plane top;
+    Linear::Plane bottom;
+    Linear::Plane left;
+    Linear::Plane right;
+  };
+
   Camera();
 
   Camera(ElemType, ElemType, ElemType);
@@ -16,24 +48,20 @@ public:
   // Rotate camera along 3 axis
   void Rotate(ElemType, ElemType, ElemType);
 
-  const Linear::Matrix<4, 4>& GetTransformMatrix();
+  void ResetRotate();
 
-  const ElemType FOV = 90;
-  ElemType aspectRatio = 1.7;
-  ElemType nearDistance = 0.1;
-  ElemType farDistance = 100.0;
+  const Linear::Matrix<4, 4>& GetTransformMatrix() const;
+
+  const Linear::Matrix<4, 4>& GetRotateMatrix() const;
+
+  Linear::Triangle Transform(Linear::Triangle&);
 
 private:
-  void MakeTransform();
+  void ConstructTransformMatrix();
 
   Linear::Matrix<4, 4> transform_matrix_;
-
-  Linear::Plane near_;
-  Linear::Plane far_;
-  Linear::Plane top_;
-  Linear::Plane bottom_;
-  Linear::Plane left_;
-  Linear::Plane right_;
+  Linear::Matrix<4, 4> rotation_matrix_;
+  ParametersHandler handler_;
 };
 
 }  // namespace Scene
